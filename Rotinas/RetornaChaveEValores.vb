1092f0c104ace2b722a30d837822fe7f4d224325
Tiago Almeida Goulart <tiago.agoulart@outlook.com>
2022-08-30 17:28:33 -0300

    Atualiza Nome

--- Rotinas/RetornaChaveEValores.vb
+++ Rotinas/RetornaChaveEValores.vb
@@ -1,4 +1,4 @@
-Sub RetornaChaveEValores()
+Sub Retorna_Valor_Total()
 'Para mais conteúdos igual a este consulte meu GitHub ou canal no YouTube:
 '» GitHub: https://github.com/t-goulart/VBA
 '» YouTube: https://www.youtube.com/channel/UC2iemIZz25SIaucByX5FpSw
